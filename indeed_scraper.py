295cdb79a297248bbeb580ba05453f338e1d5d62
Sam Noh <samnoh21@outlook.com>
2019-10-22 23:07:47 +1300

    chore(page, indeed_scraper): clean up

--- indeed_scraper.py
+++ indeed_scraper.py
@@ -6,11 +6,6 @@ from helpers import list_convert_into_int
 class IndeedScraper:
     """ IndeedScrapper Definition """
 
-    offset_list = []
-
-    def __init__(self):
-        self.get_offset_list()
-
     def get_indeed_page(self, runs, params=""):
         return Page.create(url=INDEED_URL, runs=runs, params=params)
 
@@ -22,14 +17,16 @@ class IndeedScraper:
         ]
         last_page = max(list_convert_into_int(self.get_indeed_page(runs)))
 
+        offset_list = []
         for page in range(last_page):
-            self.offset_list.append(f"{page*LIMIT}")
+            offset_list.append(f"{page*LIMIT}")
+        return offset_list
 
     def get_indeed_jobs(self):
         jobs = []
         runs = [{"find_all": ["div", {"class": "jobsearch-SerpJobCard"}]}]
 
-        # for offset in self.offset_list:
+        # for offset in self.get_offset_list():
         results = self.get_indeed_page(runs, f"&start={0}")
 
         for result in results:
@@ -38,9 +35,10 @@ class IndeedScraper:
                 company = result.find("span", {"class": "company"})
                 company_anchor = company.find("a")
                 if company_anchor is not None:
-                    company = company_anchor.string.strip()
+                    company = company_anchor.string
                 else:
-                    company = company.string.strip()
+                    company = company.string
+                company = company.strip()
                 jobs.append(f"{title} - {company}")
                 print(title, company)
             except Exception:
